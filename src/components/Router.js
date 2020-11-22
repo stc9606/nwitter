52c03e3b3e951d44725492b0698f24d1466b21a1
노승철 <s_chuli@noseungcheol-ui-MacBookAir.local>
2020-11-22 17:08:08 +0900

    Twitter Clone Coding Authentication Setup

---
+++ src/components/Router.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom"
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <Router>
+            <Switch>
+                {isLoggedIn? 
+                    <Router exact path="/">
+                        <Home/>
+                    </Router>
+                    :
+                    <Router exact path="/">
+                        <Auth/>
+                    </Router>
+                }
+            </Switch>
+        </Router>
+    )
+}
+export default AppRouter;
