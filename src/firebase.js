d29174a05b9b9045f50db28ea4ef96d12a7b695a
노승철 <s_chuli@noseungcheol-ui-MacBookAir.local>
2020-11-22 16:38:41 +0900

    Clone Coding Tiwtter Example -1

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8KUHWIr-jeOr-f986ph2dUZYofZe5lTU",
+    authDomain: "nwitter-21542.firebaseapp.com",
+    databaseURL: "https://nwitter-21542.firebaseio.com",
+    projectId: "nwitter-21542",
+    storageBucket: "nwitter-21542.appspot.com",
+    messagingSenderId: "55116701099",
+    appId: "1:55116701099:web:82e6282d2be29d342706ee"
+  };
+ 
+ export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from "./firebase";
+console.log(firebase)
 
 
 ReactDOM.render(
@@ -9,5 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
